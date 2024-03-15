dd330cdf8c33a1b9ecba5ef600235cd4b01df9b9
IlyaSTT <120708240+IlyaSTT@users.noreply.github.com>
2024-03-15 16:22:12 +0300

    Update

--- Program.cs
+++ Program.cs
@@ -1,27 +1,27 @@
-// Написать программу, которая из имеющегося массива строк формирует 
-
+ // Написать программу, которая из имеющегося массива строк формирует 
     // массив из строк, длина которых меньше либо равна 3 символа.
-
 internal class Program
-
 {
-
     static void Main(string[] args)
-
     {
-
         Console.Clear();
-
         string[] inputArray1 = {"hello", "2", "world", ":-)"};
-
         string[] inputArray2 = {"1234", "1567", "-2", "computer science"};
-
         string[] inputArray3 = {"Russia", "Denmark", "Kazan"};
 
+        string[] array1 = GetFilteredArray(inputArray1, 3);
+        string[] array2 = GetFilteredArray(inputArray2, 3);
+        string[] array3 = GetFilteredArray(inputArray3, 3);
+        
+        Console.Write(
+                     $"Примеры:\n"
+                   + $"\n{JoinArray(inputArray1, ',')} -> {JoinArray(array1, ',')}\n"
+                   + $"\n{JoinArray(inputArray2, ',')} -> {JoinArray(array2, ',')}\n"
+                   + $"\n{JoinArray(inputArray3, ',')} -> {JoinArray(array3, ',')}\n\n"
+        );
     }
 
-}
-static string[] GetFilteredArray(string[] inArray, int inLimiter)
+    static string[] GetFilteredArray(string[] inArray, int inLimiter)
     {
         int resultArraySize = 0;
         string[] resultArray = new string[resultArraySize];
@@ -36,3 +36,15 @@ static string[] GetFilteredArray(string[] inArray, int inLimiter)
         }
         return resultArray;
     }
+
+        static string JoinArray(string[] inArray, char inSeparator)
+    {
+        string resultString = "[";
+        if (inArray.Length == 0) return resultString + "]";
+        foreach (var item in inArray)
+        {
+            resultString += $"\"{item}\"{inSeparator} ";
+        }
+        return resultString + "\b\b]";
+    }
+}
